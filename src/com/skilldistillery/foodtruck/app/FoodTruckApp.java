fb0fd3a2468811a069103d3b05adbc879c658a08
mannygutierrez12 <35671250+mannygutierrez12@users.noreply.github.com>
2023-03-27 11:23:13 -0400

    Update FoodTruckApp.java

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,15 +1,18 @@
 package com.skilldistillery.foodtruck.app;
 
-import java.util.Scanner;
-
 import com.skilldistillery.foodtruck.entities.FoodTruck;
+
+
+import java.util.Scanner;
 
 public class FoodTruckApp {
     public static void main(String[] args) {
 
         Scanner kb = new Scanner(System.in);
 
-        //Created an array with the max number of truck possible
+        //Created an array with the max number of truck possible using the FoodTruck class because
+        // it is static.
+
         FoodTruck[] foodTrucks = new FoodTruck[FoodTruck.getMaxNumberOfTrucks()];
 
         //Created a reference called program Components that can get access the
@@ -24,33 +27,44 @@ public class FoodTruckApp {
             System.out.println("Please choose a number from the menu above: ");
             String userInput = kb.next();
 
-           if (userInput.equals("1")) {
+            if (userInput.equals("1")) {
                 if (count == 5) {
                     System.out.println("Sorry, but the app reached its limit");
                     continue;
                 } else {
                     String truckName;
-                    System.out.print("Please enter the name of the truck. (NO SPACES!)");
+
+                    //Was having issues with name with spaces, and decided to tell the user.
+                    System.out.println("Please enter the name of the truck without spaces");
+                    System.out.println("Type quit to exit to go back to the main menu");
                     System.out.println();
                     truckName = kb.next();
                     System.out.println();
+                    if (truckName.equals("quit")) {
+                        continue;
+                    } else {
+                        String foodType;
+                        //Same problem as line 36
+                        System.out.print("What type of food do they serve? (NO SPACES)");
+                        System.out.println();
+                        foodType = kb.next();
+                        System.out.println();
 
-                    String foodType;
-                    System.out.print("What type of food do they serve? (NO SPACES) ");
-                    foodType = kb.next();
-                    System.out.println();
+                        double truckRating;
+                        System.out.println("From 1 to 5. How would you rate them?: ");
+                        truckRating = kb.nextDouble();
 
-                    double truckRating;
-                    System.out.println("From 1 to 5. How would you rate them?: ");
-                    truckRating = kb.nextDouble();
+                        //Created a unique ID per truck. Decided to use "ABC123 rather that create
+                        // a new reference
+                        String newTruckId = "ABC123" + (count + 1);
+
+                        FoodTruck newTruck = new FoodTruck(newTruckId, truckName, foodType, truckRating);
+                        foodTrucks[count++] = newTruck;
+
+                        System.out.println("Your truck was successfully added to our database.");
+                    }
 
 
-                    String newTruckId = "XYSZ0" + (count+1);
-
-                    FoodTruck newTruck = new FoodTruck(newTruckId, truckName, foodType, truckRating);
-                    foodTrucks[count++] = newTruck;
-
-                    System.out.println("Your truck was successfully added to our list");
                 }
             } else if (userInput.equals("2")) {
                 if (count == 0) {
@@ -85,7 +99,11 @@ public class FoodTruckApp {
                     }
                     System.out.println("The food truck with the highest rating is: " + newCount);
                 }
-            } else if (userInput.equals("5")) {
+            }
+
+            //The following data was optional. The project mentioned to create a command that prompted
+            //the highest rating truck. I changed the <> signal to provide the user with the least rated truck.
+            else if (userInput.equals("5")) {
                 if (count == 0) {
                     System.out.println("Sorry but we have no data yet");
                 } else {
@@ -106,6 +124,4 @@ public class FoodTruckApp {
             }
         }
     }
-
-
 }
